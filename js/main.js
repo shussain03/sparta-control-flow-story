1784b3b759dc3c318c7813ccbb48766715f9c8d1
shussain03 <shussain3@spartaglobal.com>
2019-01-14 09:24:00 +0000

    second uplaod

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// document.write("<h1>JavaScript is an object-oriented computer programming language commonly used to create interactive effects/features within a web browser. Used in Web pages, JavaScript is a 'client-side' programming language. This means that JavaScript scripts are read, interpreted and executed in the client, which is your Web browser. By comparison, 'server-side' programming languages run on a remote computer, such as a server hosting a website.</h1>");
+//The above example demonstrates that you can write html code in JavaScript 
+
+//document - links to the html 
+
+// forward slashes only allows you to comment on a single line
+/* forward slash + asterix  allows you to comment multiple lines like this comment. This text will not be show in the browser.*/
+
+// document.write("<h1> Hello World</h1>");
+// document.write("<hr>");
+// document.write("<h1> You can write HTML using only javascript.<h1>");
+// document.write("<h2 style='color:blue;'>These words are now coloured blue. </h2>");
---
+++ js/page2.js
@@ -0,0 +1,34 @@
+// var phrase = "To be or not be a Cat"; //this is a string variable
+// /*var = variable is a container that stores a specific value. The var or variable container is used to organise data iside the javascript program.*/
+// document.write(phrase);
+// document.write(phrase);//this takes the information from the top variable and prints out 2 times
+
+// phrase = "box";
+// document.write(phrase);
+// document.write(phrase);
+// document.write(phrase);// These variable is linked to box string and prints out 3 times. 
+
+"A Cat's tail: To be or not to be a Cat!"
+var welcomeMessage = alert("Welcome to a Cat's Tail");
+var answer = ("Name your Cat to find what happens next.");
+    // String w/ double quotes
+    var occupation = ("Animal that likes to thrill");
+    var life = 9 lives; // Integer
+    var education: ("Circle of Life.") //string (of text)
+    var food: ("Mouse pie")
+    var sizeSmall; true; // boolean true/false
+
+
+    if (answer) {
+      console.log('Welcome, ${answer}')
+    } else {
+      console.log(prompt("What is your occupation?");
+    }
+
+    /*
+    Names are case-sensitive and may begin with: letters, $, _ , After, may include letters, numbers, $, _
+
+    Convention says
+         Start with a lowercase word, then additional words are capitalized
+         ex. myFirstVariable
+    */
